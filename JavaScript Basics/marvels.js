50daf9f1e60521878ee8efecb266f6c0a8ebde7c
LifeofAGeek <sethi8001@gmail.com>
2020-04-29 18:16:51 +0530

    arrays basics

---
+++ JavaScript Basics/marvels.js
@@ -0,0 +1,12 @@
+//datatypes - Arrays
+
+const arr = ["ironman", "superman","spiderman","capt. America"]
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
+console.log(`we have ${arr.length} superheros`);
+
+
+// const one = 1;
+// const two = 2;
+// console.log(`One add two is ${one + two}`); // output: One add two is 3
