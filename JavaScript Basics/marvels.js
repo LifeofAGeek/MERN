161d88aca6ed6f8bf2c4f4ca1b6c8ef54a1e5d4d
LifeofAGeek <sethi8001@gmail.com>
2020-08-09 10:20:35 +0530

    added files

--- JavaScript Basics/marvels.js
+++ JavaScript Basics/marvels.js
@@ -7,6 +7,6 @@ console.log(arr[arr.length - 1]);
 console.log(`we have ${arr.length} superheros`);
 
 
-// const one = 1;
-// const two = 2;
-// console.log(`One add two is ${one + two}`); // output: One add two is 3
+const one = 1;
+const two = 2;
+console.log(`One add two is ${one + two}`); // output: One add two is 3
