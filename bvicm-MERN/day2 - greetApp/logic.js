e443b86dab060d4213e6ec8bd4610bb3021d1f29
LifeofAGeek <sethi8001@gmail.com>
2020-08-11 21:25:16 +0530

    redirected

--- bvicm-MERN/day2 - greetApp/logic.js
+++ bvicm-MERN/day2 - greetApp/logic.js
@@ -1,3 +1,6 @@
+alert("Welcome");
+document.querySelector('#b1').addEventListener('click',sayWelcome);
+document.querySelector('#b2').addEventListener('click',clearAll);
 function initCap(name){
     return name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
 }
